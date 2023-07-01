b1b462f003e6dd12454a026b8e8f2d6501240053
cholazzzb <nicsphehehe@gmail.com>
2023-07-01 15:19:30 +0700

    feat(server): add merchant item mock api

---
+++ src/app/api/merchant/menu/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+
+import { merchants } from '~/__generated__/mock-data/merchants';
+import { Menu } from '~/domains/menu/entity';
+
+export async function GET(request: Request): Promise<
+  NextResponse<
+    | {
+        menu: Menu;
+      }
+    | { message: string }
+  >
+> {
+  const merchantID = request.headers.get('Merchant-Id');
+  if (!merchantID) {
+    return NextResponse.json(
+      { message: `merchantID is required` },
+      { status: 400 },
+    );
+  }
+
+  const menu = merchants[merchantID];
+  if (!menu) {
+    NextResponse.json(
+      { message: `merchantID ${merchantID} not found` },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({ menu }, { status: 200 });
+}
